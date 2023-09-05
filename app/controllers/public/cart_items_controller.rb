178122fc0aefa50663488b689cdabc3703b7a7a7
Ryunosuke_Ozaki <138109860+ryunosuke1990127@users.noreply.github.com>
2023-09-05 14:41:14 +0000

    update

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -8,9 +8,17 @@ class Public::CartItemsController < ApplicationController
   end
 
   def create
-    # カートに入れるが押された際の処理
-    @cart_item = CartItem.new(cart_item_params)
-    @cart_item.save
+    # 今ログインしているユーザーが追加した商品がそのユーザーのカート内にあるかチェック
+    if current_customer.cart_items.find_by(item_id: params[:cart_item] [:item_id])
+      # すでに登録がある場合はプラス
+      cart_item = CartItem.find_by(item_id: params[:cart_item] [:item_id])
+      cart_item.amount += params[:cart_item][:amount].to_i
+      cart_item.update(amount: cart_item.amount)
+    else
+      # そのユーザーのカート内に登録がない場合
+      cart_item = CartItem.new(cart_item_params)
+      cart_item.save
+    end
     redirect_to cart_items_path
   end
 
